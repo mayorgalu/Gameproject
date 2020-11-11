337ed0fa9b803219a0c68db01e03eccde28a2f1b
mayorgalu <mayorgalu@estrategy.tech>
2020-11-11 17:36:22 -0400

    Updated Storyline

---
+++ index.js
@@ -0,0 +1,26 @@
+let readlineSync = require("readline-sync");
+
+console.log(
+  "Welcome to Hogwarts School: Quiddich Tournament! Slytherin v. Griffindorf"
+);
+console.log("What do you want to do?");
+let option = readlineSync.question(
+  "Options: grab magic broom, zoom up, wait for the goldensnitch"
+);
+
+switch (option) {
+  case "grab magic broom":
+    console.log(
+      "You are now equiped with a Nimbus 2020 and are prepared to compete!"
+    );
+    break;
+  case "zoom up":
+    console.log("Wait! You can't chase the Golden Snitch without your broom!");
+    break;
+  case "wait for the goldensnitch":
+    console.log('You will never win! "Muggles can\'t fly."');
+    break;
+  default:
+    console.log("You fell off your broom!");
+    console.log('Get up, Harry!: "Quit messin\' around."');
+}
